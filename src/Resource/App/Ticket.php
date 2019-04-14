360771c58852c62bf5bd29873220a3e111b56577
Niidashi Kunihiro <nii.kunihiro@gmail.com>
2019-04-14 19:59:19 +0900

    add Ticket Resouce

---
+++ src/Resource/App/Ticket.php
@@ -0,0 +1,27 @@
+<?php
+namespace MyVendor\Ticket\Resource\App;
+
+use BEAR\Resource\Annotation\JsonSchema;
+use BEAR\Resource\ResourceObject;
+use Ray\Query\Annotation\AliasQuery;
+
+/**
+ * Class Ticket
+ */
+class Ticket extends ResourceObject
+{
+    /**
+     * @JsonSchema(key="ticket", schema="ticket.json")
+     * @AliasQuery("ticket_item_by_id", type="row")
+     *
+     * @param string $id
+     *
+     * @return ResourceObject
+     */
+    public function onGet(string $id) : ResourceObject
+    {
+        unset($id);
+
+        return $this;
+    }
+}
