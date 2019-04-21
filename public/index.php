33a1ddf8e2be453abc933e17e8963c10a47161cd
Niidashi Kunihiro <nii.kunihiro@gmail.com>
2019-04-21 22:03:29 +0900

    setting context

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 require dirname(__DIR__) . '/autoload.php';
-exit((require dirname(__DIR__) . '/bootstrap.php')(PHP_SAPI === 'cli-server' ? 'hal-api-app' : 'prod-hal-api-app'));
+/*
+ * 画面表示は html-app
+ * API は hal-api-app
+ *  API ではパスに /api/ をつける
+ * 本番環境では先頭に prod- をつける
+ */
+$context = (strpos($_SERVER['REQUEST_URI'], '/api/') === 0) ? 'hal-api-app' : 'html-app';
+exit((require dirname(__DIR__) . '/bootstrap.php')($context));
